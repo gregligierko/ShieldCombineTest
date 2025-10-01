7704728512220a4e0d356dcf14a99b52594761c0
Greg <greg@idea.org>
2025-10-01 22:23:02 +0200

    Initial Commit

---
+++ ShieldCombineTest2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShieldCombineTest2
+//
+//  Created by Greg Ligierko on 01/10/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShieldCombineTest2/ShieldCombineTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  ShieldCombineTest2App.swift
+//  ShieldCombineTest2
+//
+//  Created by Greg Ligierko on 01/10/2025.
+//
+
+import SwiftUI
+
+@main
+struct ShieldCombineTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
